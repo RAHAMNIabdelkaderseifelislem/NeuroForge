1661eed46b79def6d94ade1ff6e3cf67684416d3
Abd El Kader Seif El Islem RAHMANI <a.e.k426rahmani@gmail.com>
2024-11-10 14:38:40 +0100

    🚀 Set up Streamlit app structure
    Implemented main navigation with sidebar and page selection. Home, Data Preprocessing, Model Building, Training, and Export pages are accessible.

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
- 
+import streamlit as st
+from src.ui.pages import home, model_building, training#, data_preprocessing, export
+
+
+st.sidebar.title("NeuroForge")
+page = st.sidebar.radio("Navigation", ("Home", "Data Preprocessing", "Model Building", "Training", "Export"))
+
+if page == "Home":
+    home.display()
+elif page == "Model Building":
+    model_building.display()
+elif page == "Training":
+    training.display()
+"""
+elif page == "Data Preprocessing":
+    data_preprocessing.display()
+elif page == "Export":
+    export.display()
+"""
